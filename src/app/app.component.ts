82d637d470afd21c29042f9d3d06a0464a7dc45b
fdevinar <fc_devinar@hotmail.com>
2023-03-02 19:44:51 -0300

    basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,32 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'tic-tac-toe';
   blocks = [0,0,0,0,0,0,0,0,0]
+  blockValue = {0: '', 1: "X", 2: "O"};
+  currentPlayer = 1;
 
   select(block,idx) {
-    console.log("Block: :", block, " Index: ",idx);
+    // console.log("Block: :", block, " Index: ",idx);
+    // this.blocks[idx] = 1;
+    // console.log(this);
+
+    if (block != 0) {
+      return;
+    }
+    if (this.currentPlayer === 1) {
+      this.blocks[idx] = 1;
+      this.currentPlayer = 2;
+    } else {
+      this.blocks[idx] = 2;
+      this.currentPlayer = 1;
+    }
+
   }
+
+  reset() {
+    this.blocks = [0,0,0,0,0,0,0,0,0];
+    this.currentPlayer = 1;
+  }
+
 
 }
 
