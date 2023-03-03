a6a285b4ff826a7d8e6691a345d95391a0997017
fdevinar <fc_devinar@hotmail.com>
2023-03-02 21:22:38 -0300

    finished win conditions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,6 @@ export class AppComponent {
   currentPlayer = 1;
 
   select(block,idx) {
-    // console.log("Block: :", block, " Index: ",idx);
-    // this.blocks[idx] = 1;
-    // console.log(this);
 
     if (block != 0) {
       return;
@@ -25,6 +22,51 @@ export class AppComponent {
     } else {
       this.blocks[idx] = 2;
       this.currentPlayer = 1;
+    }
+
+    this.checkWin();
+
+  }
+
+  checkWin() {
+    let blk = this.blocks;
+
+    // Victory conditions
+
+    // top line 0 1 2
+    this.checkCondition(1,0,1,2);
+    this.checkCondition(2,0,1,2);
+    // mid line 3 4 5
+    this.checkCondition(1,3,4,5);
+    this.checkCondition(2,3,4,5);
+    // bottom line 6 7 8
+    this.checkCondition(1,6,7,8);
+    this.checkCondition(2,6,7,8);
+
+    // first column 0 3 6
+    this.checkCondition(1,0,3,6);
+    this.checkCondition(2,0,3,6);
+    // second column 1 4 7
+    this.checkCondition(1,1,4,7);
+    this.checkCondition(2,1,4,7);
+    // third column 2 5 8
+    this.checkCondition(1,2,5,8);
+    this.checkCondition(2,2,5,8);
+
+    // diagonal LR 0 4 8
+    this.checkCondition(1,0,4,8);
+    this.checkCondition(2,0,4,8);
+    // diagonal RL 2 4 6
+    this.checkCondition(1,2,4,6);
+    this.checkCondition(2,2,4,6);
+
+  }
+
+  checkCondition(player,first,second,third) {
+    let blk = this.blocks;
+    if (blk[first] === player && blk[second] === player && blk[third] === player) {
+      player === 1 ? player="X" : player="O";
+      alert(`${player} won the game!`);
     }
 
   }
